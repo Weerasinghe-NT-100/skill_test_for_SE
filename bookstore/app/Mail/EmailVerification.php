6b4029332124e6eaab7aee9cd30c7419ce393dd6
Weerasinghe-NT-100 <it20017392@my.sliit.lk>
2024-04-27 22:59:47 +0530

    Added Email varification

---
+++ bookstore/app/Mail/EmailVerification.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+
+class EmailVerification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function build()
+    {
+        return $this->subject('Email Verification')
+                    ->view('emails.email-verification');
+    }
+}
